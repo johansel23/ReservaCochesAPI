0caf51c35a0ddafbf2b27daeaf0f40fa071635d8
LAPTOP-249E9V8A\EL_JO <el_johan25@hotmail.com>
2022-03-26 12:31:58 +0100

    Se agrego el modelo de cohes. Endpoint FindAll. Se corrigieron las relaciones entre las tres tablas.

--- src/main/java/com/example/springeventos/marcas/Marca.java
+++ src/main/java/com/example/springeventos/marcas/Marca.java
@@ -1,17 +1,17 @@
 package com.example.springeventos.marcas;
 
-import java.util.List;
+import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotEmpty;
 
 import com.example.springeventos.modelo.Modelo;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -28,11 +28,8 @@ public class Marca {
     @NotEmpty
     private String nombre_marca;
     
-
-    //Relacion uno a muchos, el cual se relaciona con modelo.
-    @OneToMany()
-    @JoinColumn(name = "id_marca")
-    private List<Modelo> modelo;
-    
+    @JsonBackReference
+    @OneToMany(targetEntity = Modelo.class, mappedBy = "id_marca", orphanRemoval = false, fetch = FetchType.LAZY)
+	private Set<Modelo> modelos;
     
 }
--- src/main/java/com/example/springeventos/modelo/Modelo.java
+++ src/main/java/com/example/springeventos/modelo/Modelo.java
@@ -1,11 +1,25 @@
 package com.example.springeventos.modelo;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+
+import com.example.springeventos.coches.Coche;
+import com.example.springeventos.marcas.Marca;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import org.hibernate.FetchMode;
+import org.hibernate.annotations.Fetch;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -20,7 +34,14 @@ public class Modelo {
     @NotEmpty
     private String nombre_modelo;
     @NotNull
-    private Integer id_marca;
-
+    private Integer id_marca;      
     
+    @ManyToOne()
+	@JoinColumn(name = "id_marca", insertable = false, updatable = false)
+	private Marca marca;
+
+    @JsonBackReference
+    @OneToMany(targetEntity = Coche.class, mappedBy = "id_modelo", orphanRemoval = false, fetch = FetchType.LAZY)
+	private Set<Coche> coches;
+
 }
