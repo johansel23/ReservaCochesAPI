5956a6f0360afc9e84d85bebd5dde8bdef4a3e3c
LAPTOP-249E9V8A\EL_JO <el_johan25@hotmail.com>
2022-03-26 21:22:22 +0100

    Se agrego las relaciones entre la table coches y reserva coche

--- src/main/java/com/example/springeventos/coches/Coche.java
+++ src/main/java/com/example/springeventos/coches/Coche.java
@@ -1,16 +1,21 @@
 package com.example.springeventos.coches;
 
-import javax.persistence.CascadeType;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import com.example.springeventos.cochesreserva.CocheReserva;
 import com.example.springeventos.modelo.Modelo;
+import com.fasterxml.jackson.annotation.JsonBackReference;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -43,5 +48,9 @@ public class Coche {
     @ManyToOne()
     @JoinColumn(name = "id_modelo", insertable = false, updatable = false)
 	private Modelo modelo;
+
+    @JsonBackReference
+    @OneToMany(targetEntity = CocheReserva.class, mappedBy = "id_coche", orphanRemoval = false, fetch = FetchType.LAZY)
+	private List<CocheReserva> cocheReserva;
 
 }
